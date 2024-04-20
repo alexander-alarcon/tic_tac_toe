521e103576cbca04372c381c737bfd045f7ae09f
Alexander Alarcon <walarcon14@gmail.com>
2024-04-20 13:03:06 -0500

    feat: Add Limited game
    Add Limited memory logic

---
+++ tic_tac_toe/game/limited.py
@@ -0,0 +1,37 @@
+from queue import Queue
+
+from tic_tac_toe.game.base import BaseTicTacToe, Move
+from tic_tac_toe.player.base import Player
+
+
+class LimitedMemoryTicTacToe(BaseTicTacToe):
+    def __init__(self, player_x: Player, player_o: Player) -> None:
+        super().__init__(player_x, player_o)
+        self._memory: Queue[Move] = Queue()
+
+    def play(self) -> None:
+        super().play()
+
+    def _before_move(self, move: int) -> None:
+        """
+        Puts the move in the memory queue before the move is made.
+
+        Parameters:
+            move (int): The cell number where the move is made.
+        """
+        super()._before_move(move)
+        self._memory.put(Move(player=self._current_player, cell=move))
+
+        if self._memory.qsize() == 7:
+            self._remove_first()
+
+    def _remove_first(self) -> None:
+        """
+        Removes the first move from the memory queue and updates the board.
+        """
+        move: Move = self._memory.get()
+
+        row = (move.cell - 1) // 3
+        col = (move.cell - 1) % 3
+
+        self.board[row][col] = None
