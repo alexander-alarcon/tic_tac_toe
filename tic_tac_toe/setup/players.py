433c852adf01da1142fe7b9da09a017ff977cf26
Alexander Alarcon <walarcon14@gmail.com>
2024-04-21 10:33:54 -0500

    feat: Allow select IA as opponent

--- tic_tac_toe/setup/players.py
+++ tic_tac_toe/setup/players.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from tic_tac_toe.player.base import Player, PlayerOrNone, PlayerSymbol
 from tic_tac_toe.player.human import Human
+from tic_tac_toe.player.ia import IA, IALevel
 
 
 class SymbolChoice(Enum):
@@ -13,6 +14,12 @@ class SymbolChoice(Enum):
 class OpponentChoice(Enum):
     QUIT = 0
     HUMAN_OPPONENT = 1
+    IA_OPPONENT = 2
+
+
+class IALevelChoice(Enum):
+    QUIT = 0
+    EASY = 1
 
 
 class PlayersSetup:
@@ -56,4 +63,52 @@ class PlayersSetup:
             if self.player.symbol == PlayerSymbol.O
             else PlayerSymbol.O
         )
-        self.opponent = Human(symbol=opponent_symbol)
+
+        while True:
+            try:
+                print('\nChoose your opponent')
+                print('1. Human')
+                print('2. IA')
+                print('0. Quit')
+
+                opponent_choice = int(input('Enter your choice: '))
+
+                match opponent_choice:
+                    case OpponentChoice.HUMAN_OPPONENT.value:
+                        self.opponent = Human(symbol=opponent_symbol)
+                        break
+                    case OpponentChoice.IA_OPPONENT.value:
+                        level: IALevel = self._choose_ia_level()
+                        self.opponent = IA(symbol=opponent_symbol, level=level)
+                        break
+                    case OpponentChoice.QUIT.value:
+                        raise SystemExit
+                    case _:
+                        raise ValueError
+            except ValueError:
+                print('Invalid input. Please enter a number between 0 and 2.')
+
+    def _choose_ia_level(self) -> IALevel:
+        while True:
+            try:
+                print('\nChoose your IA level')
+                print('1. Easy')
+
+                print('0. Quit')
+
+                opponent_choice = int(input('Enter your choice: '))
+
+                match opponent_choice:
+                    case IALevelChoice.EASY.value:
+                        return IALevel.EASY
+                    case IALevelChoice.QUIT.value:
+                        raise SystemExit
+                    case _:
+                        raise ValueError
+            except ValueError:
+                min_num = min(IALevelChoice)
+                max_num = max(IALevelChoice)
+                message = (
+                    f'Please enter a number between {min_num} and {max_num}.'
+                )
+                print('Invalid input.', message)
